a2c45449a8b94776988cb356e72e3cf515a04e2c
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-29 18:59:18 +0700

    Parent nodes and child nodes

--- webdev101/js_fundamentals/dom-manipulation/domManipulation.js
+++ webdev101/js_fundamentals/dom-manipulation/domManipulation.js
@@ -10,5 +10,22 @@ const blue = document.createElement('h3');
 blue.textContent = "I\'m a blue h3!";
 blue.style.color = 'blue'
 
+// a <div> with a black border and pink 
+// background color with the following elements inside of it:
+// - another <h1> that says “I’m in a div”
+// - a <p> that says “ME TOO!”
+const blackAndPink = document.createElement('div');
+const divInADiv = document.createElement('h1');
+const pInADiv = document.createElement('p');
+
+blackAndPink.style.borderColor = 'black';
+blackAndPink.style.backgroundColor = 'pink';
+divInADiv.textContent = "I\'m in a div";
+pInADiv.textContent = "ME TOO!";
+
+blackAndPink.appendChild(divInADiv);
+blackAndPink.appendChild(pInADiv);
+
 container.appendChild(red);
 container.appendChild(blue);
+container.appendChild(blackAndPink);
